23ed7185a7c233c931c1e613fad9ab5706c17b82
Zangwei Zheng <zangwei@comp.nus.edu.sg>
2023-10-28 15:17:45 +0800

    rebuilding site Sat Oct 28 15:17:45 +08 2023

--- js/dark.js
+++ js/dark.js
@@ -4,15 +4,19 @@ document.addEventListener("DOMContentLoaded", function() {
     const button = document.querySelector(".btn-switch");
     const themeName = document.querySelector(".theme-name");
 
-    let currentTheme = "auto";
-    if (!app.classList.contains("auto")) {
-        let currentTheme = app.classList.contains("light") ? "light" : "dark";
+    let currentTheme = "auto"
+    if (app.classList.contains("auto")) {
+        app.classList.remove("auto");
+        app.classList.add("light");
+        currentTheme = "light";
+    } else {
+        currentTheme = app.classList.contains("dark") ? "light" : "dark";
     }
 
     button.addEventListener("click", function() {
         app.classList.remove(currentTheme);
-        currentTheme = currentTheme === "light" ? "dark" : "light";
-        currentThemeName = currentTheme === "light" ? "Light" : "Dark";
+        currentThemeName = currentTheme === "dark" ? "Light" : "Dark";
+        currentTheme = currentTheme === "dark" ? "light" : "dark";
         app.classList.add(currentTheme);
 
         themeName.innerText = currentThemeName;
